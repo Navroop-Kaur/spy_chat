42854080617f1fc523525c32544b9e8d555dd036
Navroop <gillnavi926@gmail.com>
2018-01-29 19:36:25 +0530

    basic spy details

---
+++ main.py
@@ -0,0 +1,9 @@
+print "Hello!"
+print "What's Up?"
+print 'Let\'s get started.'
+spy_name = raw_input("What is your name?")
+print 'Welcome ' + spy_name +' Glad to have you back with us.'
+spy_salutation = raw_input("What should i call you? Mr. or Ms.")
+spy_name = spy_salutation +" " + spy_name
+print spy_name
+print "Alright"+" " + spy_name + " "+"i would like to know more about you"
