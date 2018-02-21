5f6961cc9710c6aede3d87e3c61dc2c01de2b5cd
Navroop <gillnavi926@gmail.com>
2018-02-21 19:44:26 +0530

    history.csv created

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
-from spy_details import spy
+from spy_details import spy,friends
 from steganography.steganography import Steganography
 from datetime import datetime
+from spy_details import Spy,ChatMessage
+import csv
+
 
 print "Hello!" #welcome msgs for user
 print 'Let\'s get started.'
 STATUS_MESSAGES = ['Cant talk spychat only','Available','Always busy','At work','in a meeting']
-friends =[{'name':'raj','age':26,'rating':5.8,'is_online':True,'chats':[]} ,{'name':'simran','age':36,'rating':6.8,'is_online':True},'chats':[]]
+
+def load_frnd():
+    with open('friends.csv', 'rb') as friends_data:
+        reader = csv.reader(friends_data)
+
+        for row in reader[4:]:
+            if row:
+                name = row[0]
+                age = (row[2])
+                rating = (row[3])
+                is_online = [4]
+        for row in reader:
+            spy = Spy(name,age,rating,is_online)
+            spy = Spy(row[0],row[1],row[2],row[3])
+            friends.append(spy)
 
 
+load_frnd()
+
+def load_chats():
+        reader = list(csv.reader(chats_data))
+
+        for row in reader[4:]:
+            if row:
+                name_of_sender = row[0]
+                message_sent_to = row[1]
+                text = row[2]
+                sent_by_me = row[4]
+                new_chat = ChatMessage(name_of_sender,message_sent_to,text,sent_by_me)
+                chats.append(new_chat)
+                cm = ChatMessage(row[0],row[1])
+                chats_data.append(cm)
+
+def load_history():
+
+    with open('history.csv','rb') as history_data:
+        reader = csv.reader(history_data)
+        for row in reader:
+
+            history.append(load_chats())
+
+load_history()
 
 def add_status (C_S_M) : #function to add status C_S_M is the variable it can bt changed
     if C_S_M != None : #if C_S_M is not equal to none then print statement will execute
@@ -27,20 +69,20 @@ def add_status (C_S_M) : #function to add status C_S_M is the variable it can bt
     return new_status
 
 def  add_friend() :
-    frnd = {
-        'name': '',
-        'age': 0,
-        'rating': 0.0,
-        'is_online': True
-    }
-    frnd['name'] =  raw_input("write your name: ")
-    frnd['age'] = input("write the age of the frnd: ")
-    frnd_sal = raw_input("Mr. or Ms.: ")
-    frnd['_name'] = frnd_sal+ " " + frnd['name']
-    frnd['rating'] = input('write the rating of frnd: ')
+    frnd = Spy('','',0,0.0)
+    frnd.name =  raw_input("write your name: ")
+    frnd.age = input("write the age of the frnd: ")
+    frnd.salutation = raw_input("Mr. or Ms.: ")
+    frnd.name = frnd.salutation+ " " + frnd.name
+    frnd.rating = input('write the rating of frnd: ')
+    frnd.is_online = True
 
-    if len(frnd['name'])>2 and 50>=frnd['age']>=12 and frnd['rating']>= spy['rating']:
+    if len(frnd.name)>2 and 50>=frnd.age>=12 and frnd.rating>= spy.rating:
         friends.append(frnd)
+        with open('friends.csv', 'a') as friends_data:
+            writer = csv.writer(friends_data)
+            writer.writerow([frnd.name, frnd.rating, frnd.age, frnd.is_online])
+
 
     else:
         print("frnd with these values cant be added")
@@ -82,7 +124,7 @@ def read_message() :
     friends[choosen_frnd]['chats'].append(new_chat)
     print "Your secret message is " + secret_text
 
-def spy_chat(spy_name,spy_age,spy_rating): #function is created with name spy_chat
+def spy_chat(spy): #function is created with name spy_chat
 
     current_status_message = None #initally there is no current status
     show_menu = True #menu will be displayed only when value is true
@@ -106,40 +148,35 @@ def spy_chat(spy_name,spy_age,spy_rating): #function is created with name spy_ch
 question = raw_input(" Are you a new user? Y or N :") #getting input from user to know that if user is new
 if question.upper() == "N": #.upper class changes the input into upper case
     print "We already have your details...glad to see you again" #if user says no then a msg will be displayed.
-    spy_chat(spy['name'],spy['age'],spy['rating'])#function uses values from spy_details
+    spy_chat(spy)#function uses values from spy_details
 elif question.upper() =="Y": #if user says yes then he needs to provide this details
-    spy = {
-        'name': '',
-        'age': 0 ,
-        'rating': 0.0,
-        'is_online': True
-    }
+    spy = Spy('','',0,0.0)
     print "What's Up?" #"" '' both types of inverted commas can be used to print a statement
-    spy['name'] = raw_input("What is your name?")#raw_input gets string values from user
-    if len(spy['name'])>3: #if name length is greater than 3 only then user will be allowed to enter the system
-        print 'Welcome ' + spy['name'] +' Glad to have you back with us.' #concatenation of strings
-        spy_salutation = raw_input("What should i call you? Mr. or Ms.")
-        if spy_salutation == "Mr." or spy_salutation == "Ms." or spy_salutation == "mr." or spy_salutation == "ms.": #no other value will be used for salutaion
-            spy['name'] = spy_salutation +" " + spy['name'] #" " is used to provide space
-            print spy['name']
-            print "Alright" + " " + spy['name'] + " " + "i would like to know more about you before we proceed..."
+    spy.name = raw_input("What is your name?")#raw_input gets string values from user
+    if len(spy.name)>3: #if name length is greater than 3 only then user will be allowed to enter the system
+        print 'Welcome ' + spy.name +' Glad to have you back with us.' #concatenation of strings
+        spy.salutation = raw_input("What should i call you? Mr. or Ms.")
+        if spy.salutation == "Mr." or spy.salutation == "Ms." or spy.salutation == "mr." or spy.salutation == "ms.": #no other value will be used for salutaion
+            spy.name = spy.salutation +" " + spy.name #" " is used to provide space
+            print spy.name
+            print "Alright" + " " + spy.name + " " + "i would like to know more about you before we proceed..."
         else:
             print "Enter a valid slutation" #if any salutaion other than the salutaions mentioned in if statement like rr then this msg will display
 
-        spy['age'] = input("What is your age?") #input is used to get integer values from user
-        if spy['age'] > 12 and spy['age'] < 50: #if entered age is btw 12 and 50 only then next input will be asked for
-            spy['rating'] = input("What is your spy rating?")
-            if spy['rating'] > 4.5: # gradation of spies on basis of rating
+        spy.age = input("What is your age?") #input is used to get integer values from user
+        if spy.age > 12 and spy.age < 50: #if entered age is btw 12 and 50 only then next input will be asked for
+            spy.rating = input("What is your spy rating?")
+            if spy.rating > 4.5: # gradation of spies on basis of rating
                 print 'best spy...!'
-            elif spy['rating'] > 3.5 and spy['rating'] <= 4.5:
+            elif spy.rating > 3.5 and spy.rating <= 4.5:
                 print 'good spy...you can do much better'
-            elif spy['rating'] >= 2.5 and spy['rating'] <= 3.5:
+            elif spy.rating >= 2.5 and spy.rating <= 3.5:
                 print 'fine spy..need to do better'
 
-                spy['is_online'] = True #after entering all the details now spy is online
-                print "Authentication complete. Welcome " + spy['name'] + " age: " + str(spy['age']) + " and rating of: " + str(
-                    spy['rating']) + " Proud to have you onboard" #str() is used to join integers and strings or for typecasting
-                spy_chat(spy['name'], spy['age'], spy['rating'])#all the values will be passed to fun spy_chat
+                spy.is_online = True #after entering all the details now spy is online
+                print "Authentication complete. Welcome " + spy.name + " age: " + str(spy.age) + " and rating of: " + str(
+                    spy.rating) + " Proud to have you onboard" #str() is used to join integers and strings or for typecasting
+                spy_chat(spy.name, spy.age, spy.rating)#all the values will be passed to fun spy_chat
         else:
             print "Your age is not valid"#age less than 12 or greater than 50
             print 'We can always use somebody to help in the office.'
--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,36 @@
-spy ={
-    'name' : 'Navroop',
-    'age' : 24,
-    'rating' : 2.5,
-    'is_online' : True
-}
+from datetime import datetime
+class Spy:
+    def __init__(self, name, salutation, age, rating):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_online = True
+        self.chats = []
+        self.current_status_message = None
+
+spy = Spy("Navroop",'Ms.',22,2.4)
+
+friend_one = Spy('Raja', 'Mr.', 27, 4.9)
+friend_two = Spy('Mata Hari', 'Ms.', 21, 4.39)
+friend_three = Spy('No', 'Dr.', 37, 4.95)
+
+friends = [friend_one, friend_two, friend_three]
+
+
+class ChatMessage:
+    def __init__(self, sender,message_sent_to,message, sent_by_me):
+        self.name_of_sender = sender
+        self.message_sent_to = message_sent_to
+
+    def __init__(self,message, sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
+
+
+cm = ChatMessage("message","sent_by_me")
+chat_one = ChatMessage("message","sent_by_me")
+chat_two = ChatMessage("message","sent_by_me")
+chat_three = ChatMessage("message","sent_by_me")
+chats =[chat_one,chat_two,chat_three]
